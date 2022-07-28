5c593b8f5fbcb124e8b25ebb75ad1eb12bfa08f0
Rodrigo <gokusan@keemail.me>
2022-07-27 21:40:50 -0500

    dont remember :D

--- client/src/components/Contacto.jsx
+++ client/src/components/Contacto.jsx
@@ -1,22 +1,43 @@
 import {BigBox, Container, Form, PinkBox} from './Contacto.style';
-import useFormName from '../utils/useFormName'; //FIXME: hacer otro?
+import { useState } from 'react';
+import { func } from 'prop-types';
+// import useFormName from '../utils/useFormName'; //FIXME: hacer otro?
 
 export default function Contacto() { 
-  const register = (e) => { //FIXME: hacer funcionar
-    e.preventDefault();
-    console.log({ values });
-  }
+  // const register = (e) => { //FIXME: hacer funcionar
+  //   e.preventDefault();
+  // }
 
-  const [values, handleChange, handleSubmit] = useFormName(register);
+  // const [values, handleChange, handleSubmit] = useFormName(register);
+  const [contact, setContact] = useState({
+    email: '',
+    number: '',
+  })
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
 
   return (
     <BigBox>
       <img src="../../imgs/Screenshot from 2022-06-07 19-29-51.png" alt="agent_pic" />
       <Container>
         <h3>Datos de Contacto</h3>
-        <Form onSubmit={handleSubmit}> {/* FIXME aqui no va a ir boton creo que lo debo pasar per props a otro componente*/}
-          <input name={values.email} type='email' placeholder='Correo Electronico' onChange={handleChange} />
-          <input name={values.phone} type='number' placeholder='Telefono' onChange={handleChange} />
+        <Form onSubmit='#'> {/* FIXME aqui no va a ir boton creo que lo debo pasar poahsr props a otro componente*/}
+          <input
+            name='number'
+            type='email'
+            placeholder='Correo Electronico'
+            onChange={handleChange}
+            value={contact.email}
+          />
+          <input 
+            name='number'
+            type='number'
+            placeholder='Telefono'
+            onChange={handleChange}
+            value={contact.number}
+          />
           <input type='submit' value='Continuar' /> {/* FIXME aqui no va a ir boton creo que lo debo pasar per props a otro componente*/}
         </Form>
         <PinkBox isTrue={false} />
